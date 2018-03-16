7c393a7dd47ddce4776c320bf799d9a61e48e566
Markus Dethlefsen <markus.dethlefsen@stylelounge.de>
2018-03-17 00:59:11 +0100

    fix snapshot testing for modals

--- src/components/MainMenu/MainMenu.test.js
+++ src/components/MainMenu/MainMenu.test.js
@@ -1,18 +1,12 @@
-import initStoryshots, { getSnapshotFileName } from '@storybook/addon-storyshots'
-import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 
-initStoryshots({
+import initStoryshots from '@storybook/addon-storyshots'
 
-    storyKindRegex: /^MainMenu$/,
-    test: ({ story, context }) => {
-        const snapshotFileName = getSnapshotFileName(context)
-        const storyElement = story.render(context)
-        const shallowTree = shallow(storyElement)
-
-        if (snapshotFileName) {
-            expect(toJson(shallowTree)).toMatchSpecificSnapshot(snapshotFileName)
-        }
+/*
+jest.mock('material-ui/Modal', () => {
+    return function Modal(props) {
+        return props.children
     }
 })
+*/
 
+initStoryshots({ storyKindRegex: /^MainMenu$/ })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,14 @@
-import Enzyme from 'enzyme'
+import enzyme from 'enzyme/build/index'
 import Adapter from 'enzyme-adapter-react-16'
 
 process.env.NODE_ENV = 'test'
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 240000
 
-Enzyme.configure({ adapter: new Adapter() })
+enzyme.configure({ adapter: new Adapter() })
+
+jest.mock('material-ui/Modal', () => {
+    return function Modal(props) {
+        return props.children
+    }
+})
 
